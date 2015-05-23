7aef982b250a0f4e63f1daadab78e3abf082af1b
eferri65 <elenarodellaferri@gmail.com>
2015-05-23 13:18:55 -0400

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,67 @@
+library("plyr")
+
+## set your root -- assumption you are in UCI HAR Dataset
+root = "/Users/rodele/Desktop/UCI HAR Dataset"
+setwd(root)
+
+##----------------------1
+##Merges the training and the test sets to create one data set.
+##Both training and test sets have the same 561 features columns, they only differ for 
+##the subjects being measured: 9 for test and 21 for train
+##I will merge the 2 data sets like "stacking" one on top of the other so that we get the 
+##561 features on the complete universe of 30 subjects
+
+##get the activity and features data common for the test and train data
+activity_labels<-read.table("activity_labels.txt")
+features<-read.table("features.txt", stringsAsFactors = FALSE ) 
+
+##get the test data
+setwd("test") 
+X_test<-read.table("X_test.txt")
+y_test<-read.table("y_test.txt")
+y_test<-rename(y_test, c("V1"="Activity"))
+subject_test = read.table("subject_test.txt")
+subject_test<-rename(subject_test, c("V1"="Subject"))
+test<-cbind(subject_test, y_test, X_test)
+
+##get the train data
+setwd(root)
+setwd("train") 
+X_train<-read.table("X_train.txt")
+y_train<-read.table("y_train.txt")
+y_train<-rename(y_train, c("V1"="Activity"))
+subject_train<-read.table("subject_train.txt")
+subject_train<-rename(subject_train, c("V1"="Subject"))
+train<-cbind(subject_train, y_train, X_train)
+
+##clip test and train data: they have the SAME columns
+tt<-rbind(train,test)
+
+##----------------------2
+##Extracts only the measurements on the mean and standard deviation for each measurement. 
+##First name the columns using the feautures frame
+default.colnames<-colnames(tt)
+for(i in 3:ncol(tt) ) {  
+        colnames(tt)[colnames(tt)==default.colnames[i]] <- features[i-2, "V2"]
+}
+##Case sensitive
+tt.subset<-tt[ , grepl("Subject|Activity|std|mean", colnames(tt))]
+
+##----------------------3
+##Uses descriptive activity names to name the activities in the data set
+tt.subset$Activity<-activity_labels[,2][match(tt$Activity, activity_labels[,1])]
+
+##----------------------4
+##Appropriately labels the data set with descriptive variable names. 
+##Make the labels more descriptive 
+
+##----------------------5
+##From the data set in step 4, creates a second, independent tidy data set with the average of 
+##each variable for each activity and each subject.
+groupColumns<-c("Activity","Subject")
+tt_subset_summary = ddply(tt.subset, groupColumns, colwise(mean))
+
+##----------------------6
+##Upload tt_subset_summary set as a txt file created with write.table() using row.name=FALSE 
+setwd(root)
+write.table(tt_subset_summary, "final_dataset", row.names = FALSE)
